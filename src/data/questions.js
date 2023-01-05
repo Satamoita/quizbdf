56b2f430e54c30f0c78c048c813ccbf1ac248ec3
Satamoita <122056817+Satamoita@users.noreply.github.com>
2023-01-05 18:16:36 -0300

    BDF

---
+++ src/data/questions.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    question: "Quem e o mais corno?",
+    options: [
+      "Eu",
+      "todo mundo",
+      "Felipe",
+      "Jelado",
+    ],
+    answer: "todo mundo",
+  },
+  {
+    question: "Quem bebia mais quando era mais novo:",
+    options: ["Dr fofo", "Felipe Nojeira", "Sana corno", "Pedro pelos"],
+    answer: "Dr fofo",
+  },
+  {
+    question: "Quem e o mais broxa",
+    options: ["Gustavao", "Felipe", "Giovannao", "Jelado"],
+    answer: "Gustavao",
+  },
+  {
+    question: "Para aumento peniano, qual o melhor produto",
+    options: ["libid gel", "maca peruana", "viagra", "couve flor"],
+    answer: "libid gel",
+  },
+  {
+    question: "Quem peida mais tenebroso é o",
+    options: ["Gustavao", "Giovannao whey", "Pedrao pelos", "Dr fofo kibe"],
+    answer: "Gustavao",
+  },
+];
+
+export default data;
---
+++ src/data/questions_complete.js
@@ -0,0 +1,94 @@
+const data = [
+  {
+    category: "Corno",
+    questions: [
+      {
+        question: "Quem e o mais corno",
+        options: ["Eu", "todo mundo", "Felipe", "Jelado"],
+        answer: "todo mundo",
+        tip: "É todo mundo cornao",
+      },
+      {
+        question: "Quem e o mais feio",
+        options: ["Jelado", "Jeladeira", "Jeladinho", "turzin"],
+        answer: "Jeladeira",
+        tip: "Jeladeira nojento so de olhar eu vomito",
+      },
+      {
+        question: "Quem bebia mais quand quando era mais novo:",
+        options: ["Dr fofo", "Felipe Nojeira", "Sana corno", "Pedro pelos"],
+        answer: "Dr fofo",
+      },
+      {
+        question: "Quem daria o cu por 1 160cc",
+        options: ["Giovannao", "Gustavao", "Pedrao Luca", "Jelado"],
+        answer: "Jelado",
+        tip: "Jeladeira da o cu até por um prato de comida"
+      },
+      {
+        question: "Quem e o mais broxa",
+        options: ["Gustavao", "Felipe", "Giovannao", "Jelado"],
+        answer: "Gustavao",
+      },
+    ],
+  },
+  {
+    category: "Corpo",
+    questions: [
+      {
+        question: "Quem tem o maior pe do grupo",
+        options: ["Andre abomineve", "Gustavo", "Dr fofo", "Pedrao"],
+        answer: "Andre abomineve",
+        tip: "Ele mora nas neves",
+      },
+      {
+        question: "Para aumento peniano, qual o melhor produto",
+        options: ["libid gel", "maca peruana", "viagra", "couve flor"],
+        answer: "libid gel",
+      },
+      {
+        question: "Quem peida mais tenebroso e",
+        options: ["Gustavao", "Giovannao whey", "Pedrao pelos", "Dr fofo kibe"],
+        answer: "Gustavao",
+      },
+    ],
+  },
+  {
+    category: "Comedor de casada",
+    questions: [
+      {
+        question: "Quem e o comedor de casada do grupo?",
+        options: [
+          "Felipe Dls",
+          "Felipe dela libera",
+          "Felipe rei do ferro",
+          "Rei do ferro",
+        ],
+        answer: "Felipe rei do ferro",
+      },
+      {
+        question: "Quem come mais gorda",
+        options: ["Felipe", "Jelado", "Pedrao paraguaio", "thurzin"],
+        answer: "Felipe",
+      },
+      {
+        question: "Quem tem mais chances de comer um travesti",
+        options: ["Felipe", "Gustavo Sana", "Pedro Luca", "o grupo todo"],
+        answer: "o grupo todo",
+      },
+      {
+        question: "Quem daria o cuzin por um prato de arroz com 2 bife",
+        options: ["jelado", "Luis henrique", "Frost", "Frozen afro"],
+        answer: "jelado",
+      },
+      {
+        question:
+          "Escolha um guardiao pra te proteger",
+        options: ["cido milani", "pele", "rainha bet", "ze piu"],
+        answer: "ze piu",
+      },
+    ],
+  },
+];
+
+export default data;
